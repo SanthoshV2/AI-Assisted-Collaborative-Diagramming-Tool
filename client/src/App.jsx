80431720fbc2d1bd113010698ee1e4ad60bdb9f3
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-09-11 03:23:27 +0530

    Created the DiagramCanvas for the platform

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from "@clerk/clerk-react";
 import Home from "./Components/pages/Home.jsx";
 import Dashboard from "./Components/pages/Dashboard.jsx";
+import DiagramCanvas from "./Components/canvas/DiagramCanvas.jsx";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/canvas/:id" element={<DiagramCanvas />} />
         </Routes>
        </div>
       </BrowserRouter>
