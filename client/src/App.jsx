9e7b102444d891192cb0a8604ed9ececdb6bf03c
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-09-11 02:19:22 +0530

    Designed the langing page for the platfrom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,40 +2,28 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import Home from "./Components/pages/Home.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <BrowserRouter>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline text-blue-500 decoration-blue-500">
-        Vite + React
-      </h1>
-      <h1 className="text-4xl font-extrabold text-green-600 underline decoration-pink-500">
-        Tailwind CSS Test
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="bg-yellow-200 text-black p-4 rounded-lg mt-4">
-        If you see this yellow box, Tailwind CSS is working!
-      </p>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here, e.g. <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+       </div>
+      </BrowserRouter>
     </>
   );
 }
